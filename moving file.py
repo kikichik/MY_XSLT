b036ada27d2f85bb8c2fa85a06c4b289d89872db
NGurev <Nikita.Gurev@lanit-tercom.com>
2019-08-26 18:47:41 +0300

    function for adding meta tags in xslt

--- moving file.py
+++ moving file.py
@@ -22,6 +22,7 @@ def search_talon(path_to_folder):
     print('selected cct:', cct_found[index_chosen - 1])
     current_dir = os.path.join(path_to_simiFolders, cct_found[index_chosen - 1])
 
+#меняем  исходную папку
 def change_current_folder():
     global path_to_simiFolders
     print('print folder:')
@@ -173,23 +174,3 @@ if __name__ == '__main__':
 
 
 
-
-
-
-
-
-
-
-
-
-# для всех файлов
-# if dirs_in_simi !='':
-#     for dir_first_level in dirs_in_simi:
-#         #папки второго уровня: cct, doc, forms, xml, xslt, xslt_screen
-#         for dir_second_level in os.listdir((os.path.join(path_to_simiFolders, dir_first_level))):
-#             if dir_second_level == 'xslt_screen':
-#                 # сохраняем путь к папке xslt_screen
-#                 path_to_xs = os.path.join(path_to_simiFolders, dir_first_level, dir_second_level)
-#                 path_first_level = os.path.join(path_to_simiFolders, dir_first_level)
-#                 for files in os.listdir(path_to_xs):
-#                     print(files)
